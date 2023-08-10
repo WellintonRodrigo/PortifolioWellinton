dbef05d2f1a058916fa8329e669c64520ac6d3d0
Wellinton Rodrigo Silva <wellinton.rodrigo26@gmail.com>
2023-08-09 22:56:52 -0300

    :lipstick: feat: Atualizações dos arquivos

--- LANDING PAGE - portfólio/asset/js/script.js
+++ LANDING PAGE - portfólio/asset/js/script.js
@@ -1,12 +1,23 @@
-function active_words(elemento){
-    let arrText= elemento.innerHTML.split('')
-    elemento.innerHTML=''
-    arrText.forEach((letra, i)=>{
-        setTimeout(()=>{
-            elemento.innerHTML += letra
-        }, 75 * i)
-    })
+
+function Escrevendo(){
+
+    function active_words(elemento){
+        let arrText= elemento.innerHTML.split('')
+        elemento.innerHTML=''
+        arrText.forEach((letra, i)=>{
+            setTimeout(()=>{
+                elemento.innerHTML += letra
+            }, 75 * i)
+        })
+    }
+    
+    const titulo= document.querySelector('.digitando')
+    active_words(titulo)
+
 }
 
-const titulo= document.querySelector('.digitando')
-active_words(titulo)
+const menu = document.querySelector('.menu-bars')
+
+menu.addEventListener('click',()=>{
+    menu.classList.toggle('fa-x')
+})
